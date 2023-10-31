8789c65ddf40247b5805efbc389d144a68e12877
Cristina Mateo <cris.mateo.99@gmail.com>
2023-10-31 22:26:51 +0100

    dos gráficas con color

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+let calories = [];
+let fat = [];
+let sugar = [];
+let carbs = [];
+let protein = [];
+let nombre = [];
+
+
+
+
+
 const cardTemplate = function (image, fruit) {
     return `<div class="card">
                 <img src="${image}" alt="${fruit}" class="fruitimg">
@@ -7,15 +19,91 @@ const cardTemplate = function (image, fruit) {
 
   const fruitsNode = document.getElementById("frutas");
 
+  const api = 'https://www.fruityvice.com/api/fruit/all'
 
-  fetch('https://www.fruityvice.com/api/fruit/all')
-        .then(res => res.json())
-        .then(elements => {
 
-      let cards = ""
+  async function getFruits() {
+    let response = await fetch(api);
+    let data = await response.json();
+  
+        let cards = ""
+  
+      for (let i = 0; i < data.length; i++) {
+       cards+= cardTemplate(`./assets/${data[i].name}.jpg`, data[i].name)
+      }
+      fruitsNode.innerHTML = cards
+  
+  
+      for(let i = 0; i < data.length; i++){
+          calories.push(data[i].nutritions.calories)
+          fat.push(data[i].nutritions.fat)  
+          sugar.push(data[i].nutritions.sugar)  
+          carbs.push(data[i].nutritions.carbohydrates)  
+          protein.push(data[i].nutritions.protein)
+          nombre.push(data[i].name)  
+    
+        }
+        generarGrafica()
+       
 
-    for (let i = 0; i < elements.length; i++) {
-     cards+= cardTemplate(`./assets/${elements[i].name}.jpg`, elements[i].name)
-    }
-    fruitsNode.innerHTML = cards
-    });
+  }
+  
+getFruits()
+
+
+function generarGrafica(){
+const sugarchart = document.getElementById('chart1');
+
+new Chart(sugarchart, {
+  type: 'bar',
+  data: {
+    labels: nombre,
+    datasets: [{
+      label: 'sugar in fruits',
+      data: sugar,
+      borderWidth: 1,
+      backgroundColor: [
+        'rgb(178, 208, 94)',
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+
+  }
+});
+
+
+const caloriesChart = document.getElementById('chart2');
+
+new Chart(caloriesChart, {
+  type: 'bar',
+  data: {
+    labels: nombre,
+    datasets: [{
+      label: 'protein in fruits',
+      data: protein,
+      borderWidth: 1,
+      backgroundColor: [
+        'rgb(253, 181, 93)',
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+
+  }
+});
+
+
+}
+      
+
